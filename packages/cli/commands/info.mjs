3cba90a8444e0e1d34fc530470eb6f35c7fc2abf
Eduardo Wesley <eduardomudutiu@gmail.com>
2022-04-06 21:30:24 -0300

    feat: add info command

---
+++ packages/cli/commands/info.mjs
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+import { packagesInfoPath } from '../config/paths.mjs';
+
+const info = (packageName) => {
+  const packagesInfo = JSON.parse(fs.readFileSync(packagesInfoPath, 'utf-8'));
+  const packageInfo = packagesInfo[packageName];
+
+  if (!packageInfo) {
+    console.log(chalk.red(`Package ${packageName} not found in the local registry`));
+    process.exit(1);
+  }
+
+  console.log(chalk.cyan(JSON.stringify(packageInfo, null, 2)));
+};
+
+export const command = 'info <packageName>';
+export const aliases = [];
+export const desc = 'Returns info about a (local) published package';
+export const builder = {};
+export const handler = (argv) => info(argv.packageName);
