f1f024fff4e6b2b7abb3009814754c97e12d81f6
Eduardo Wesley <eduardomudutiu@gmail.com>
2022-04-09 18:41:15 -0300

    fix: show the right version using --version flag

--- packages/cli/config/paths.mjs
+++ packages/cli/config/paths.mjs
@@ -1,6 +1,11 @@
 import path from 'path';
 import os from 'os';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { version } = require('../package.json');
 
 export const packageDir = path.join(os.homedir(), '.loctry');
 export const modulesRoot = path.join(packageDir, 'node_modules');
 export const packagesInfoPath = path.join(packageDir, 'packages-info.json');
+export const currentVersion = version;
